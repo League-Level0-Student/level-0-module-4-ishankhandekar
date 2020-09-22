c364d6ae6b0d992297bba415c5746786bedcd835
ishankhandekar <66933296+ishankhandekar@users.noreply.github.com>
2020-09-22 16:55:51 -0700

    finished googly eyes  started piAloud

--- src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
+++ src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
@@ -1,11 +1,29 @@
-
-void setup() {
   PImage face;
+void setup() {
+
   face = loadImage ("bigeyes.jpg");
   size(800,600);
-  face.resize(400,400);
+  face.resize(800,600);
+ 
+  image(face, 0, 0);
 }
 
 void draw() {
-  
+  image(face, 0, 0);
+   int X = (mouseX);
+  int Y = (mouseY);
+    println(mouseX,mouseY);
+    noStroke();
+    fill(#FFFFFF);
+ellipse(447,343,100,130);
+ellipse(207, 351, 100, 130);
+fill(#050505);
+if( X >= 172 && X <= 244 && Y >= 305 && Y <= 391 ){
+  X = mouseX;
+  Y = mouseY;
+}else{ X = 202;
+  Y = 346;}
+  ellipse(X, Y, 50, 50);
+  ellipse(X + 250, Y, 50, 50);
+
 }
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -13,9 +13,11 @@ public class PiAloud {
 	
 	
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+	String pie = "3.1415926535897932384";
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
 
@@ -32,7 +34,7 @@ public class PiAloud {
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" 
 	
-	
+}
 	
 	/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
